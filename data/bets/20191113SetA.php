c0f4ff3de007800072709fc856ba96ef4a3845d8
Adil EL Bourichi <adilelbourichi@gmail.com>
2019-11-13 10:23:05 +0800

    Added bets for 13 November 2019

---
+++ data/bets/20191113SetA.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+	'R1' => [
+		/**
+		Race 1
+		*/
+		'WIN' => [2, 1, 7, 8, 3, 6, 9],
+		'PLACE' => [2, 1, 7, 8, 3, 6, 9],
+		'QUINELLA PLACE' => [2, 1, 7, 8, 3, 6, 9],
+		'QUINELLA' => [2, 1, 7, 8, 3, 6, 9],
+		'FIRST 4' => [2, 1, 7, 8, 3, 6, 9],
+		'TRIO' => [2, 1, 7, 8, 3, 6, 9],
+		'TIERCE' => [2, 1, 7, 8, 3, 6, 9],
+		'winBets' => 700,
+		'unitWinBet' => 100,
+		'plaBets' => 700,
+		'unitPlaBet' => 100,
+		'qplBets' => 210,
+		'unitQplBet' => 10,
+		'qinBets' => 210,
+		'unitQinBet' => 10,
+		'f4Bets' => 350,
+		'trioBets' => 350,
+		'tceBets' => 210,
+		'totalBets' => 2730
+	],
+	'R2' => [
+		/**
+		Race 2
+		*/
+		'WIN' => [7, 3, 9, 5, 11, 12, 10],
+		'PLACE' => [7, 3, 9, 5, 11, 12, 10],
+		'QUINELLA PLACE' => [7, 3, 9, 5, 11, 12, 10],
+		'QUINELLA' => [7, 3, 9, 5, 11, 12, 10],
+		'FIRST 4' => [7, 3, 9, 5, 11, 12, 10],
+		'TRIO' => [7, 3, 9, 5, 11, 12, 10],
+		'TIERCE' => [7, 3, 9, 5, 11, 12, 10],
+		'winBets' => 700,
+		'unitWinBet' => 100,
+		'plaBets' => 700,
+		'unitPlaBet' => 100,
+		'qplBets' => 210,
+		'unitQplBet' => 10,
+		'qinBets' => 210,
+		'unitQinBet' => 10,
+		'f4Bets' => 350,
+		'trioBets' => 350,
+		'tceBets' => 210,
+		'totalBets' => 2730
+	],
+];
---
+++ data/bets/20191113SetD.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+	'R1' => [
+		/**
+		Race 1
+		*/
+		'WIN' => [2, 1, 7],
+		'PLACE' => [2, 1, 7],
+		'QUINELLA PLACE' => [2, 1, 7],
+		'QUINELLA' => [2, 1, 7],
+		'FIRST 4' => [],
+		'TRIO' => [2, 1, 7],
+		'TIERCE' => [2, 1, 7],
+		'winBets' => 300,
+		'unitWinBet' => 100,
+		'plaBets' => 300,
+		'unitPlaBet' => 100,
+		'qplBets' => 30,
+		'unitQplBet' => 10,
+		'qinBets' => 30,
+		'unitQinBet' => 10,
+		'f4Bets' => 0,
+		'trioBets' => 10,
+		'tceBets' => 60,
+		'totalBets' => 730
+	],
+	'R2' => [
+		/**
+		Race 2
+		*/
+		'WIN' => [7, 3, 9, 5],
+		'PLACE' => [7, 3, 9, 5],
+		'QUINELLA PLACE' => [7, 3, 9, 5],
+		'QUINELLA' => [7, 3, 9, 5],
+		'FIRST 4' => [7, 3, 9, 5],
+		'TRIO' => [7, 3, 9, 5],
+		'TIERCE' => [7, 3, 9, 5],
+		'winBets' => 400,
+		'unitWinBet' => 100,
+		'plaBets' => 400,
+		'unitPlaBet' => 100,
+		'qplBets' => 60,
+		'unitQplBet' => 10,
+		'qinBets' => 60,
+		'unitQinBet' => 10,
+		'f4Bets' => 10,
+		'trioBets' => 40,
+		'tceBets' => 240,
+		'totalBets' => 1210
+	],
+];
