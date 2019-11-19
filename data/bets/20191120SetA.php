28dc335e6be77e41a39abed504148d6698f6cda2
Adil EL Bourichi <adilelbourichi@gmail.com>
2019-11-19 19:58:49 +0800

    Results for 11/17 and bets for 11/20

---
+++ data/bets/20191120SetA.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+	'R1' => [
+		/**
+		Race 1
+		*/
+		'WIN' => [4, 2, 1, 8, 10, 7],
+		'PLACE' => [4, 2, 1, 8, 10, 7],
+		'QUINELLA PLACE' => [4, 2, 1, 8, 10, 7],
+		'QUINELLA' => [4, 2, 1, 8, 10, 7],
+		'FIRST 4' => [4, 2, 1, 8, 10, 7],
+		'TRIO' => [4, 2, 1, 8, 10, 7],
+		'TIERCE' => [4, 2, 1, 8, 10, 7],
+		'winBets' => 600,
+		'unitWinBet' => 100,
+		'plaBets' => 600,
+		'unitPlaBet' => 100,
+		'qplBets' => 150,
+		'unitQplBet' => 10,
+		'qinBets' => 150,
+		'unitQinBet' => 10,
+		'f4Bets' => 150,
+		'trioBets' => 200,
+		'tceBets' => 120,
+		'totalBets' => 1970
+	],
+	'R2' => [
+		/**
+		Race 2
+		*/
+		'WIN' => [12, 1, 4, 5, 11, 10],
+		'PLACE' => [12, 1, 4, 5, 11, 10],
+		'QUINELLA PLACE' => [12, 1, 4, 5, 11, 10],
+		'QUINELLA' => [12, 1, 4, 5, 11, 10],
+		'FIRST 4' => [12, 1, 4, 5, 11, 10],
+		'TRIO' => [12, 1, 4, 5, 11, 10],
+		'TIERCE' => [12, 1, 4, 5, 11, 10],
+		'winBets' => 600,
+		'unitWinBet' => 100,
+		'plaBets' => 600,
+		'unitPlaBet' => 100,
+		'qplBets' => 150,
+		'unitQplBet' => 10,
+		'qinBets' => 150,
+		'unitQinBet' => 10,
+		'f4Bets' => 150,
+		'trioBets' => 200,
+		'tceBets' => 120,
+		'totalBets' => 1970
+	],
+];
