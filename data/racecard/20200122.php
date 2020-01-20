d6a39e90f2c75b793366cefba92b629522db8232
adoula4141 <elbourichi.adil@gmail.com>
2020-01-20 07:18:40 +0000

    Added bets for 01/22/2020

---
+++ data/racecard/20200122.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+	 Jockey Names
+*/
+return [
+	1 => [
+		/**
+		Race 1
+		*/
+		1 => "KHChan",
+		2 => "ZPurton",
+		3 => "LHewitson",
+		4 => "HWLai",
+		5 => "KCLeung",
+		6 => "SdeSousa",
+		7 => "TPiccone",
+		8 => "BShinn",
+		9 => "KTeetan",
+		10 => "CYHo",
+		11 => "NCallan",
+		12 => "JMoreira",
+	],
+	2 => [
+		/**
+		Race 2
+		*/
+		1 => "CYHo",
+		2 => "HTMo",
+		3 => "LHewitson",
+		4 => "MFPoon",
+		5 => "JMoreira",
+		6 => "BShinn",
+		7 => "NCallan",
+		8 => "HWLai",
+		9 => "TPiccone",
+		10 => "ZPurton",
+		11 => "KTeetan",
+		12 => "SdeSousa",
+	],
+	3 => [
+		/**
+		Race 3
+		*/
+		1 => "BShinn",
+		2 => "KHChan",
+		3 => "HWLai",
+		4 => "CYHo",
+		5 => "LHewitson",
+		6 => "HTMo",
+		7 => "THSo",
+		8 => "HNWong",
+		9 => "TPiccone",
+		10 => "KCLeung",
+		11 => "JMoreira",
+		12 => "MFPoon",
+	],
+	4 => [
+		/**
+		Race 4
+		*/
+		1 => "THSo",
+		2 => "JMoreira",
+		3 => "KCLeung",
+		4 => "HTMo",
+		5 => "KTeetan",
+		6 => "NCallan",
+		7 => "LHewitson",
+		8 => "CYHo",
+		9 => "MFPoon",
+		10 => "SdeSousa",
+		11 => "HWLai",
+		12 => "HNWong",
+	],
+	5 => [
+		/**
+		Race 5
+		*/
+		1 => "NCallan",
+		2 => "KHChan",
+		3 => "TPiccone",
+		4 => "CYHo",
+		5 => "BShinn",
+		6 => "THSo",
+		7 => "HNWong",
+		8 => "JMoreira",
+		9 => "KTeetan",
+		10 => "KCLeung",
+		11 => "SdeSousa",
+		12 => "HTMo",
+	],
+	6 => [
+		/**
+		Race 6
+		*/
+		1 => "THSo",
+		2 => "HTMo",
+		3 => "KTeetan",
+		4 => "JMoreira",
+		5 => "LHewitson",
+		6 => "BShinn",
+		7 => "TPiccone",
+		8 => "CYHo",
+		9 => "KHChan",
+		10 => "NCallan",
+		11 => "KCLeung",
+		12 => "HWLai",
+	],
+	7 => [
+		/**
+		Race 7
+		*/
+		1 => "KHChan",
+		2 => "CYHo",
+		3 => "TPiccone",
+		4 => "BShinn",
+		5 => "HWLai",
+		6 => "JMoreira",
+		7 => "SdeSousa",
+		8 => "KTeetan",
+		9 => "KCLeung",
+		10 => "LHewitson",
+		11 => "THSo",
+		12 => "MFPoon",
+	],
+	8 => [
+		/**
+		Race 8
+		*/
+		1 => "BShinn",
+		2 => "TPiccone",
+		3 => "KTeetan",
+		4 => "ZPurton",
+		5 => "NCallan",
+		6 => "MFPoon",
+		7 => "KHChan",
+		8 => "HTMo",
+		9 => "JMoreira",
+		10 => "KCLeung",
+		11 => "CYHo",
+		12 => "SdeSousa",
+	],
+	9 => [
+		/**
+		Race 9
+		*/
+		1 => "KHChan",
+		2 => "KTeetan",
+		3 => "MFPoon",
+		4 => "ZPurton",
+		5 => "THSo",
+		6 => "BShinn",
+		7 => "TPiccone",
+		8 => "KCLeung",
+		9 => "CYHo",
+		10 => "HTMo",
+		11 => "JMoreira",
+		12 => "SdeSousa",
+	],
+];
