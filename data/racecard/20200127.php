2a25084b291a9598af3fb4e2623441c9d6928d32
adoula4141 <elbourichi.adil@gmail.com>
2020-01-23 08:21:14 +0000

    Added bets for 01/27/2020

---
+++ data/racecard/20200127.php
@@ -0,0 +1,208 @@
+<?php
+
+/**
+	 Jockey Names
+*/
+return [
+	1 => [
+		/**
+		Race 1
+		*/
+		1 => "KHChan",
+		2 => "TPiccone",
+		3 => "ABadel",
+		4 => "HWLai",
+		5 => "BShinn",
+		6 => "HNWong",
+		7 => "KCLeung",
+		8 => "KTeetan",
+		9 => "CYHo",
+		10 => "ZPurton",
+		11 => "LHewitson",
+		12 => "JMoreira",
+		13 => "NCallan",
+		14 => "THSo",
+	],
+	2 => [
+		/**
+		Race 2
+		*/
+		1 => "BShinn",
+		2 => "ZPurton",
+		3 => "SdeSousa",
+		4 => "JMoreira",
+		5 => "MFPoon",
+		6 => "MChadwick",
+		7 => "RMoore",
+		8 => "HTMo",
+		9 => "ABadel",
+		10 => "NCallan",
+		11 => "CYHo",
+		12 => "LHewitson",
+		13 => "KTeetan",
+		14 => "KCLeung",
+	],
+	3 => [
+		/**
+		Race 3
+		*/
+		1 => "JMoreira",
+		2 => "ZPurton",
+		3 => "KHChan",
+		4 => "RMoore",
+		5 => "MLYeung",
+		6 => "KCLeung",
+		7 => "MFPoon",
+		8 => "HTMo",
+		9 => "NCallan",
+		10 => "LHewitson",
+		11 => "MChadwick",
+		12 => "HWLai",
+		13 => "THSo",
+		14 => "HNWong",
+	],
+	4 => [
+		/**
+		Race 4
+		*/
+		1 => "ZPurton",
+		2 => "SdeSousa",
+		3 => "TPiccone",
+		4 => "KCLeung",
+		5 => "MChadwick",
+		6 => "MFPoon",
+		7 => "JSpencer",
+		8 => "CYHo",
+		9 => "JMoreira",
+		10 => "KTeetan",
+		11 => "ABadel",
+	],
+	5 => [
+		/**
+		Race 5
+		*/
+		1 => "HWLai",
+		2 => "JSpencer",
+		3 => "ZPurton",
+		4 => "KTeetan",
+		5 => "CYHo",
+		6 => "TPiccone",
+		7 => "HTMo",
+		8 => "JMoreira",
+		9 => "MLYeung",
+		10 => "MFPoon",
+		11 => "ABadel",
+		12 => "MChadwick",
+		13 => "LHewitson",
+		14 => "HNWong",
+	],
+	6 => [
+		/**
+		Race 6
+		*/
+		1 => "TPiccone",
+		2 => "MChadwick",
+		3 => "CYHo",
+		4 => "ZPurton",
+		5 => "KTeetan",
+		6 => "BShinn",
+		7 => "LHewitson",
+		8 => "SdeSousa",
+		9 => "HNWong",
+		10 => "HWLai",
+		11 => "MLYeung",
+		12 => "KCLeung",
+		13 => "ABadel",
+		14 => "JMoreira",
+	],
+	7 => [
+		/**
+		Race 7
+		*/
+		1 => "LHewitson",
+		2 => "CYHo",
+		3 => "TPiccone",
+		4 => "ZPurton",
+		5 => "ABadel",
+		6 => "KCLeung",
+		7 => "NCallan",
+		8 => "KHChan",
+		9 => "SdeSousa",
+		10 => "HTMo",
+		11 => "MLYeung",
+		12 => "KTeetan",
+		13 => "THSo",
+		14 => "JMoreira",
+	],
+	8 => [
+		/**
+		Race 8
+		*/
+		1 => "CYHo",
+		2 => "KTeetan",
+		3 => "JMoreira",
+		4 => "RMoore",
+		5 => "SdeSousa",
+		6 => "BShinn",
+		7 => "KCLeung",
+		8 => "ZPurton",
+		9 => "NCallan",
+	],
+	9 => [
+		/**
+		Race 9
+		*/
+		1 => "KCLeung",
+		2 => "JSpencer",
+		3 => "THSo",
+		4 => "JMoreira",
+		5 => "TPiccone",
+		6 => "SdeSousa",
+		7 => "CYHo",
+		8 => "RMoore",
+		9 => "ZPurton",
+		10 => "MChadwick",
+		11 => "KTeetan",
+		12 => "KHChan",
+		13 => "ABadel",
+		14 => "HWLai",
+	],
+	10 => [
+		/**
+		Race 10
+		*/
+		1 => "KHChan",
+		2 => "LHewitson",
+		3 => "JMoreira",
+		4 => "BShinn",
+		5 => "MFPoon",
+		6 => "RMoore",
+		7 => "ZPurton",
+		8 => "KTeetan",
+		9 => "CYHo",
+		10 => "KCLeung",
+		11 => "MLYeung",
+		12 => "MChadwick",
+		13 => "ABadel",
+		14 => "SdeSousa",
+	],
+	11 => [
+		/**
+		Race 11
+		*/
+		1 => "ZPurton",
+		2 => "ABadel",
+		3 => "SdeSousa",
+		4 => "JMoreira",
+		5 => "KHChan",
+		6 => "BShinn",
+		7 => "KTeetan",
+		8 => "CYHo",
+		9 => "HTMo",
+		10 => "MChadwick",
+		11 => "THSo",
+		12 => "LHewitson",
+		13 => "MFPoon",
+		14 => "HNWong",
+	],
+];
