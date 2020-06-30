21ae3777b9b9f88f0037c3add40e5fe0eda99226
adoula4141 <elbourichi.adil@gmail.com>
2020-06-30 04:24:23 +0000

    20200701

---
+++ data/racecard/20200627.php
@@ -0,0 +1,194 @@
+<?php
+
+/**
+	 Jockey Names
+*/
+return [
+	1 => [
+		/**
+		Race 1
+		*/
+		1 => "MLYeung",
+		2 => "NCallan",
+		3 => "CYHo",
+		4 => "CSchofield",
+		5 => "ZPurton",
+		6 => "CLChau",
+		7 => "KTeetan",
+		8 => "KCLeung",
+		9 => "JMoreira",
+		10 => "VBorges",
+		11 => "AHamelin",
+		12 => "MChadwick",
+		13 => "HTMo",
+		14 => "HNWong",
+	],
+	2 => [
+		/**
+		Race 2
+		*/
+		1 => "AHamelin",
+		2 => "ZPurton",
+		3 => "KHChan",
+		4 => "MLYeung",
+		5 => "CSchofield",
+		6 => "HNWong",
+		7 => "MFPoon",
+		8 => "HTMo",
+		9 => "CYHo",
+		10 => "NCallan",
+		11 => "BShinn",
+		12 => "KTeetan",
+		13 => "JMoreira",
+		14 => "CWong",
+	],
+	3 => [
+		/**
+		Race 3
+		*/
+		1 => "KTeetan",
+		2 => "BShinn",
+		3 => "MFPoon",
+		4 => "MLYeung",
+		5 => "KCLeung",
+		6 => "HNWong",
+		7 => "ZPurton",
+		8 => "VBorges",
+		9 => "AHamelin",
+		10 => "CSchofield",
+		11 => "JMoreira",
+		12 => "NCallan",
+		13 => "HTMo",
+		14 => "THSo",
+	],
+	4 => [
+		/**
+		Race 4
+		*/
+		1 => "KHChan",
+		2 => "BShinn",
+		3 => "ZPurton",
+		4 => "CSchofield",
+		5 => "KTeetan",
+		6 => "JMoreira",
+		7 => "VBorges",
+		8 => "HNWong",
+		9 => "NCallan",
+		10 => "CLChau",
+		11 => "AHamelin",
+		12 => "CYHo",
+		13 => "MFPoon",
+		14 => "MLYeung",
+	],
+	5 => [
+		/**
+		Race 5
+		*/
+		1 => "NCallan",
+		2 => "AHamelin",
+		3 => "KTeetan",
+		4 => "JMoreira",
+		5 => "HTMo",
+		6 => "CYHo",
+		7 => "MFPoon",
+		8 => "KHChan",
+		9 => "ZPurton",
+		10 => "THSo",
+		11 => "CLChau",
+		12 => "KCLeung",
+		13 => "MChadwick",
+		14 => "VBorges",
+	],
+	6 => [
+		/**
+		Race 6
+		*/
+		1 => "AHamelin",
+		2 => "ZPurton",
+		3 => "JMoreira",
+		4 => "KTeetan",
+		5 => "CSchofield",
+		6 => "MChadwick",
+		7 => "CLChau",
+		8 => "VBorges",
+		9 => "CYHo",
+		10 => "KCLeung",
+		11 => "MLYeung",
+		12 => "MFPoon",
+		13 => "KHChan",
+		14 => "HTMo",
+	],
+	7 => [
+		/**
+		Race 7
+		*/
+		1 => "ZPurton",
+		2 => "VBorges",
+		3 => "KTeetan",
+		4 => "MChadwick",
+		5 => "CSchofield",
+		6 => "AHamelin",
+		7 => "JMoreira",
+		8 => "HTMo",
+		9 => "CYHo",
+		10 => "KHChan",
+		11 => "KCLeung",
+		12 => "CLChau",
+	],
+	8 => [
+		/**
+		Race 8
+		*/
+		1 => "ZPurton",
+		2 => "KHChan",
+		3 => "CSchofield",
+		4 => "MLYeung",
+		5 => "AHamelin",
+		6 => "CYHo",
+		7 => "KTeetan",
+		8 => "VBorges",
+		9 => "HNWong",
+		10 => "HWLai",
+		11 => "THSo",
+		12 => "HTMo",
+		13 => "JMoreira",
+	],
+	9 => [
+		/**
+		Race 9
+		*/
+		1 => "NCallan",
+		2 => "CYHo",
+		3 => "KCLeung",
+		4 => "KHChan",
+		5 => "CLChau",
+		6 => "VBorges",
+		7 => "CWong",
+		8 => "AHamelin",
+		9 => "ZPurton",
+		10 => "THSo",
+		11 => "JMoreira",
+		12 => "MFPoon",
+		13 => "HNWong",
+		14 => "KTeetan",
+	],
+	10 => [
+		/**
+		Race 10
+		*/
+		1 => "CLChau",
+		2 => "AHamelin",
+		3 => "ZPurton",
+		4 => "KCLeung",
+		5 => "VBorges",
+		6 => "NCallan",
+		7 => "KHChan",
+		8 => "JMoreira",
+		9 => "MLYeung",
+		10 => "CSchofield",
+		11 => "CYHo",
+		12 => "HNWong",
+		13 => "KTeetan",
+		14 => "MFPoon",
+	],
+];
---
+++ data/racecard/20200701.php
@@ -0,0 +1,181 @@
+<?php
+
+/**
+	 Jockey Names
+*/
+return [
+	1 => [
+		/**
+		Race 1
+		*/
+		1 => "CYHo",
+		2 => "CLChau",
+		3 => "KHChan",
+		4 => "THSo",
+		5 => "NCallan",
+		6 => "HTMo",
+		7 => "MFPoon",
+		8 => "AHamelin",
+		9 => "KTeetan",
+		10 => "JMoreira",
+		11 => "ZPurton",
+		12 => "MLYeung",
+	],
+	2 => [
+		/**
+		Race 2
+		*/
+		1 => "CWong",
+		2 => "ZPurton",
+		3 => "MFPoon",
+		4 => "HNWong",
+		5 => "MLYeung",
+		6 => "VBorges",
+		7 => "AHamelin",
+		8 => "CLChau",
+		9 => "CYHo",
+		10 => "NCallan",
+		11 => "KHChan",
+		12 => "KTeetan",
+		13 => "JMoreira",
+		14 => "MChadwick",
+	],
+	3 => [
+		/**
+		Race 3
+		*/
+		1 => "JMoreira",
+		2 => "ZPurton",
+		3 => "AHamelin",
+		4 => "HTMo",
+		5 => "BShinn",
+		6 => "KTeetan",
+		7 => "NCallan",
+		8 => "CYHo",
+		9 => "THSo",
+		10 => "HWLai",
+		11 => "KCLeung",
+		12 => "CLChau",
+		13 => "MFPoon",
+		14 => "MLYeung",
+	],
+	4 => [
+		/**
+		Race 4
+		*/
+		1 => "MFPoon",
+		2 => "KCLeung",
+		3 => "NCallan",
+		4 => "AHamelin",
+		5 => "CYHo",
+		6 => "THSo",
+		7 => "ZPurton",
+		8 => "KHChan",
+		9 => "CLChau",
+		10 => "JMoreira",
+		11 => "KTeetan",
+		12 => "HNWong",
+		13 => "HWLai",
+		14 => "MLYeung",
+	],
+	5 => [
+		/**
+		Race 5
+		*/
+		1 => "ZPurton",
+		2 => "KTeetan",
+		3 => "HNWong",
+		4 => "JMoreira",
+		5 => "MChadwick",
+		6 => "NCallan",
+		7 => "AHamelin",
+		8 => "CYHo",
+		9 => "BShinn",
+		10 => "VBorges",
+		11 => "MFPoon",
+		12 => "MLYeung",
+	],
+	6 => [
+		/**
+		Race 6
+		*/
+		1 => "ZPurton",
+		2 => "CLChau",
+		3 => "CYHo",
+		4 => "NCallan",
+		5 => "KHChan",
+		6 => "KTeetan",
+		7 => "THSo",
+		8 => "VBorges",
+		9 => "MChadwick",
+		10 => "AHamelin",
+		11 => "HNWong",
+		12 => "JMoreira",
+	],
+	7 => [
+		/**
+		Race 7
+		*/
+		1 => "JMoreira",
+		2 => "ZPurton",
+		3 => "NCallan",
+		4 => "AHamelin",
+		5 => "KTeetan",
+		6 => "THSo",
+		7 => "CLChau",
+		8 => "VBorges",
+		9 => "HWLai",
+		10 => "MLYeung",
+	],
+	8 => [
+		/**
+		Race 8
+		*/
+		1 => "NCallan",
+		2 => "CYHo",
+		3 => "ZPurton",
+		4 => "KHChan",
+		5 => "AHamelin",
+		6 => "KTeetan",
+		7 => "JMoreira",
+		8 => "THSo",
+		9 => "MLYeung",
+		10 => "MFPoon",
+		11 => "VBorges",
+	],
+	9 => [
+		/**
+		Race 9
+		*/
+		1 => "CLChau",
+		2 => "MFPoon",
+		3 => "KTeetan",
+		4 => "AHamelin",
+		5 => "MChadwick",
+		7 => "ZPurton",
+		8 => "CWong",
+		9 => "CYHo",
+		10 => "VBorges",
+		11 => "THSo",
+		12 => "JMoreira",
+	],
+	10 => [
+		/**
+		Race 10
+		*/
+		1 => "BShinn",
+		2 => "CLChau",
+		3 => "CWong",
+		4 => "JMoreira",
+		5 => "VBorges",
+		6 => "KCLeung",
+		7 => "KHChan",
+		8 => "NCallan",
+		9 => "ZPurton",
+		10 => "MFPoon",
+		11 => "KTeetan",
+		12 => "MLYeung",
+		13 => "HNWong",
+		14 => "CYHo",
+	],
+];
