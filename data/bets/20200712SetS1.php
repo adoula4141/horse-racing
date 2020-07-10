135f3bcfb5882b514e38cbde80d10099692da509
adoula4141 <elbourichi.adil@gmail.com>
2020-07-10 00:39:58 +0000

    20200712

---
+++ data/bets/20200712SetS1.php
@@ -0,0 +1,136 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'11' => [
+		/**
+		Race 11
+		*/
+		'PLACE' => [6, 9, 10],
+		'QUINELLA PLACE' => [6, 9, 10],
+		'TRIO' => [1, 3, 4, 6, 9],
+		'TIERCE' => [1, 3, 4, 6, 9, 10, 11],
+		'FIRST 4' => [1, 3, 4, 6, 9, 10, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+];
---
+++ data/racecard/20200712.php
@@ -0,0 +1,207 @@
+<?php
+
+/**
+	 Jockey Names
+*/
+return [
+	1 => [
+		/**
+		Race 1
+		*/
+		1 => "AHamelin",
+		2 => "CSchofield",
+		3 => "ZPurton",
+		4 => "BShinn",
+		5 => "HTMo",
+		6 => "KTeetan",
+		7 => "JMoreira",
+		8 => "HWLai",
+		9 => "NCallan",
+		10 => "MFPoon",
+		11 => "KHChan",
+		12 => "MLYeung",
+		13 => "HNWong",
+		14 => "CWong",
+	],
+	2 => [
+		/**
+		Race 2
+		*/
+		1 => "NCallan",
+		2 => "JMoreira",
+		3 => "CLChau",
+		4 => "BShinn",
+		5 => "ZPurton",
+		6 => "KTeetan",
+		7 => "AHamelin",
+		8 => "KCLeung",
+		9 => "MFPoon",
+		10 => "MChadwick",
+		11 => "KHChan",
+		12 => "THSo",
+		13 => "HNWong",
+		14 => "MLYeung",
+	],
+	3 => [
+		/**
+		Race 3
+		*/
+		1 => "ZPurton",
+		2 => "NCallan",
+		3 => "KTeetan",
+		4 => "CLChau",
+		5 => "MLYeung",
+		6 => "CSchofield",
+		7 => "AHamelin",
+		8 => "CWong",
+		9 => "HWLai",
+		10 => "KHChan",
+		11 => "HNWong",
+		12 => "JMoreira",
+		13 => "MFPoon",
+		14 => "HTMo",
+	],
+	4 => [
+		/**
+		Race 4
+		*/
+		1 => "MFPoon",
+		2 => "ZPurton",
+		3 => "JMoreira",
+		4 => "CLChau",
+		5 => "CSchofield",
+		6 => "KCLeung",
+		7 => "MLYeung",
+		8 => "KTeetan",
+	],
+	5 => [
+		/**
+		Race 5
+		*/
+		1 => "CLChau",
+		2 => "KCLeung",
+		3 => "BShinn",
+		4 => "CWong",
+		5 => "AHamelin",
+		6 => "ZPurton",
+		7 => "CSchofield",
+		8 => "THSo",
+		9 => "JMoreira",
+		10 => "NCallan",
+		11 => "MFPoon",
+		12 => "MLYeung",
+		13 => "KTeetan",
+		14 => "MChadwick",
+	],
+	6 => [
+		/**
+		Race 6
+		*/
+		1 => "CLChau",
+		2 => "ZPurton",
+		3 => "JMoreira",
+		4 => "CSchofield",
+		5 => "KHChan",
+		6 => "KCLeung",
+		7 => "NCallan",
+		8 => "THSo",
+		9 => "AHamelin",
+		10 => "HTMo",
+		11 => "MChadwick",
+		12 => "CWong",
+		13 => "HNWong",
+		14 => "KTeetan",
+	],
+	7 => [
+		/**
+		Race 7
+		*/
+		1 => "AHamelin",
+		2 => "ZPurton",
+		3 => "HTMo",
+		4 => "NCallan",
+		5 => "CSchofield",
+		6 => "THSo",
+		7 => "KCLeung",
+		8 => "BShinn",
+		9 => "KTeetan",
+		10 => "MChadwick",
+		11 => "HWLai",
+		12 => "JMoreira",
+		13 => "MLYeung",
+		14 => "MFPoon",
+	],
+	8 => [
+		/**
+		Race 8
+		*/
+		1 => "CLChau",
+		2 => "JMoreira",
+		3 => "MChadwick",
+		4 => "CSchofield",
+		5 => "ZPurton",
+		6 => "MLYeung",
+		7 => "KTeetan",
+		8 => "AHamelin",
+		9 => "KHChan",
+		10 => "CWong",
+		11 => "KCLeung",
+		12 => "MFPoon",
+		13 => "HTMo",
+		14 => "THSo",
+	],
+	9 => [
+		/**
+		Race 9
+		*/
+		1 => "CLChau",
+		2 => "ZPurton",
+		4 => "AHamelin",
+		5 => "HNWong",
+		6 => "JMoreira",
+		7 => "MLYeung",
+		8 => "KHChan",
+		9 => "MChadwick",
+		10 => "KTeetan",
+		11 => "MFPoon",
+		12 => "HTMo",
+	],
+	10 => [
+		/**
+		Race 10
+		*/
+		1 => "NCallan",
+		2 => "CSchofield",
+		3 => "HTMo",
+		4 => "KTeetan",
+		5 => "AHamelin",
+		6 => "KCLeung",
+		7 => "MChadwick",
+		8 => "MFPoon",
+		9 => "ZPurton",
+		10 => "CLChau",
+		11 => "HWLai",
+		12 => "JMoreira",
+		13 => "THSo",
+		14 => "MLYeung",
+	],
+	11 => [
+		/**
+		Race 11
+		*/
+		1 => "KHChan",
+		2 => "JMoreira",
+		3 => "KTeetan",
+		4 => "CLChau",
+		5 => "NCallan",
+		6 => "BShinn",
+		7 => "AHamelin",
+		8 => "CSchofield",
+		9 => "ZPurton",
+		10 => "MFPoon",
+		11 => "THSo",
+		12 => "MLYeung",
+		13 => "KCLeung",
+		14 => "HTMo",
+	],
+];
