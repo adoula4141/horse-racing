2f439d4007abaec4dc2e515d568434c43b1b8979
adoula4141 <elbourichi.adil@gmail.com>
2020-07-14 04:37:47 +0000

    20200715

---
+++ data/bets/20200715SetS1.php
@@ -0,0 +1,112 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'PLACE' => [5, 9, 10],
+		'QUINELLA PLACE' => [5, 9, 10],
+		'TRIO' => [2, 3, 5, 7, 8],
+		'TIERCE' => [2, 3, 5, 7, 8, 9, 10],
+		'FIRST 4' => [2, 3, 5, 7, 8, 9, 10],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'PLACE' => [5, 9, 10],
+		'QUINELLA PLACE' => [5, 9, 10],
+		'TRIO' => [2, 3, 5, 7, 8],
+		'TIERCE' => [2, 3, 5, 7, 8, 9, 10],
+		'FIRST 4' => [2, 3, 5, 7, 8, 9, 10],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'PLACE' => [5, 9, 10],
+		'QUINELLA PLACE' => [5, 9, 10],
+		'TRIO' => [2, 3, 5, 7, 8],
+		'TIERCE' => [2, 3, 5, 7, 8, 9, 10],
+		'FIRST 4' => [2, 3, 5, 7, 8, 9, 10],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'PLACE' => [5, 9, 10],
+		'QUINELLA PLACE' => [5, 9, 10],
+		'TRIO' => [2, 3, 5, 7, 8],
+		'TIERCE' => [2, 3, 5, 7, 8, 9, 10],
+		'FIRST 4' => [2, 3, 5, 7, 8, 9, 10],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'PLACE' => [5, 9, 10],
+		'QUINELLA PLACE' => [5, 9, 10],
+		'TRIO' => [2, 3, 5, 7, 8],
+		'TIERCE' => [2, 3, 5, 7, 8, 9, 10],
+		'FIRST 4' => [2, 3, 5, 7, 8, 9, 10],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'PLACE' => [5, 9, 10],
+		'QUINELLA PLACE' => [5, 9, 10],
+		'TRIO' => [2, 3, 5, 7, 8],
+		'TIERCE' => [2, 3, 5, 7, 8, 9, 10],
+		'FIRST 4' => [2, 3, 5, 7, 8, 9, 10],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'PLACE' => [5, 9, 10],
+		'QUINELLA PLACE' => [5, 9, 10],
+		'TRIO' => [2, 3, 5, 7, 8],
+		'TIERCE' => [2, 3, 5, 7, 8, 9, 10],
+		'FIRST 4' => [2, 3, 5, 7, 8, 9, 10],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'PLACE' => [5, 9, 10],
+		'QUINELLA PLACE' => [5, 9, 10],
+		'TRIO' => [2, 3, 5, 7, 8],
+		'TIERCE' => [2, 3, 5, 7, 8, 9, 10],
+		'FIRST 4' => [2, 3, 5, 7, 8, 9, 10],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'PLACE' => [5, 9, 10],
+		'QUINELLA PLACE' => [5, 9, 10],
+		'TRIO' => [2, 3, 5, 7, 8],
+		'TIERCE' => [2, 3, 5, 7, 8, 9, 10],
+		'FIRST 4' => [2, 3, 5, 7, 8, 9, 10],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+];
---
+++ data/racecard/20200715.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+	 Jockey Names
+*/
+return [
+	1 => [
+		/**
+		Race 1
+		*/
+		1 => "CSchofield",
+		2 => "ZPurton",
+		3 => "VBorges",
+		4 => "CLChau",
+		5 => "KCLeung",
+		6 => "KTeetan",
+		7 => "HWLai",
+		8 => "MLYeung",
+		9 => "MChadwick",
+		10 => "THSo",
+		11 => "HTMo",
+		12 => "CWong",
+	],
+	2 => [
+		/**
+		Race 2
+		*/
+		1 => "HNWong",
+		2 => "CWong",
+		3 => "CYHo",
+		4 => "ZPurton",
+		5 => "BShinn",
+		6 => "CLChau",
+		7 => "CSchofield",
+		8 => "VBorges",
+		9 => "KCLeung",
+		10 => "KHChan",
+		11 => "KTeetan",
+		12 => "HWLai",
+	],
+	3 => [
+		/**
+		Race 3
+		*/
+		1 => "ZPurton",
+		2 => "HTMo",
+		3 => "CYHo",
+		4 => "VBorges",
+		5 => "KCLeung",
+		6 => "CWong",
+		7 => "CSchofield",
+		8 => "KTeetan",
+		9 => "MChadwick",
+		10 => "MFPoon",
+		11 => "MLYeung",
+		12 => "KHChan",
+	],
+	4 => [
+		/**
+		Race 4
+		*/
+		1 => "CLChau",
+		2 => "ZPurton",
+		3 => "BShinn",
+		4 => "MChadwick",
+		5 => "CYHo",
+		6 => "MLYeung",
+		7 => "KTeetan",
+		8 => "CSchofield",
+		9 => "KCLeung",
+		10 => "KHChan",
+		11 => "VBorges",
+		12 => "MFPoon",
+	],
+	5 => [
+		/**
+		Race 5
+		*/
+		1 => "BShinn",
+		2 => "KHChan",
+		3 => "CLChau",
+		4 => "MLYeung",
+		5 => "KTeetan",
+		6 => "ZPurton",
+		7 => "THSo",
+		8 => "CSchofield",
+		9 => "VBorges",
+		10 => "KCLeung",
+		11 => "MFPoon",
+		12 => "CYHo",
+	],
+	6 => [
+		/**
+		Race 6
+		*/
+		1 => "CWong",
+		2 => "KTeetan",
+		3 => "CLChau",
+		4 => "THSo",
+		5 => "VBorges",
+		6 => "ZPurton",
+		7 => "KHChan",
+		8 => "MFPoon",
+		9 => "KCLeung",
+		10 => "HWLai",
+		11 => "MChadwick",
+		12 => "CYHo",
+	],
+	7 => [
+		/**
+		Race 7
+		*/
+		1 => "BShinn",
+		2 => "KHChan",
+		3 => "ZPurton",
+		4 => "CLChau",
+		5 => "KCLeung",
+		6 => "VBorges",
+		7 => "CYHo",
+		8 => "THSo",
+		9 => "MLYeung",
+		10 => "CSchofield",
+		11 => "KTeetan",
+		12 => "CWong",
+	],
+	8 => [
+		/**
+		Race 8
+		*/
+		1 => "CLChau",
+		2 => "BShinn",
+		3 => "KCLeung",
+		4 => "ZPurton",
+		5 => "CSchofield",
+		6 => "HTMo",
+		7 => "MFPoon",
+		8 => "CYHo",
+		9 => "VBorges",
+		10 => "MLYeung",
+		11 => "KTeetan",
+		12 => "THSo",
+	],
+	9 => [
+		/**
+		Race 9
+		*/
+		1 => "CSchofield",
+		2 => "CLChau",
+		3 => "KTeetan",
+		4 => "ZPurton",
+		5 => "CWong",
+		6 => "THSo",
+		7 => "MChadwick",
+		8 => "KHChan",
+		9 => "VBorges",
+		10 => "HTMo",
+		11 => "CYHo",
+		12 => "MFPoon",
+	],
+];
