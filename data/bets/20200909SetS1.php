fb99147ec5d360064ec756eb1e11430ee65a2587
adoula4141 <elbourichi.adil@gmail.com>
2020-09-07 06:52:14 +0000

    bets for 9/9

---
+++ data/bets/20200909SetS1.php
@@ -0,0 +1,100 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'PLACE' => [2, 4, 5],
+		'QUINELLA PLACE' => [2, 4, 5],
+		'TRIO' => [2, 3, 4, 5, 11],
+		'TIERCE' => [2, 3, 4, 5, 11],
+		'FIRST 4' => [2, 3, 4, 5, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'PLACE' => [2, 4, 5],
+		'QUINELLA PLACE' => [2, 4, 5],
+		'TRIO' => [2, 3, 4, 5, 11],
+		'TIERCE' => [2, 3, 4, 5, 11],
+		'FIRST 4' => [2, 3, 4, 5, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'PLACE' => [2, 4, 5],
+		'QUINELLA PLACE' => [2, 4, 5],
+		'TRIO' => [2, 3, 4, 5, 11],
+		'TIERCE' => [2, 3, 4, 5, 11],
+		'FIRST 4' => [2, 3, 4, 5, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'PLACE' => [2, 4, 5],
+		'QUINELLA PLACE' => [2, 4, 5],
+		'TRIO' => [2, 3, 4, 5, 11],
+		'TIERCE' => [2, 3, 4, 5, 11],
+		'FIRST 4' => [2, 3, 4, 5, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'PLACE' => [2, 4, 5],
+		'QUINELLA PLACE' => [2, 4, 5],
+		'TRIO' => [2, 3, 4, 5, 11],
+		'TIERCE' => [2, 3, 4, 5, 11],
+		'FIRST 4' => [2, 3, 4, 5, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'PLACE' => [2, 4, 5],
+		'QUINELLA PLACE' => [2, 4, 5],
+		'TRIO' => [2, 3, 4, 5, 11],
+		'TIERCE' => [2, 3, 4, 5, 11],
+		'FIRST 4' => [2, 3, 4, 5, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'PLACE' => [2, 4, 5],
+		'QUINELLA PLACE' => [2, 4, 5],
+		'TRIO' => [2, 3, 4, 5, 11],
+		'TIERCE' => [2, 3, 4, 5, 11],
+		'FIRST 4' => [2, 3, 4, 5, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'PLACE' => [2, 4, 5],
+		'QUINELLA PLACE' => [2, 4, 5],
+		'TRIO' => [2, 3, 4, 5, 11],
+		'TIERCE' => [2, 3, 4, 5, 11],
+		'FIRST 4' => [2, 3, 4, 5, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+];
