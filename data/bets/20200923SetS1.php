262f1dd76b1e4adb8ac36c4c45fec19ed8af8439
adoula4141 <elbourichi.adil@gmail.com>
2020-09-21 10:08:40 +0000

    bets for 0923

---
+++ data/bets/20200923SetS1.php
@@ -0,0 +1,100 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'PLACE' => [3, 9, 12],
+		'QUINELLA PLACE' => [3, 9, 12],
+		'TRIO' => [1, 3, 6, 7, 9],
+		'TIERCE' => [1, 3, 6, 7, 9, 12],
+		'FIRST 4' => [1, 3, 6, 7, 9, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'PLACE' => [3, 9, 12],
+		'QUINELLA PLACE' => [3, 9, 12],
+		'TRIO' => [1, 3, 6, 7, 9],
+		'TIERCE' => [1, 3, 6, 7, 9, 12],
+		'FIRST 4' => [1, 3, 6, 7, 9, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'PLACE' => [3, 9, 12],
+		'QUINELLA PLACE' => [3, 9, 12],
+		'TRIO' => [1, 3, 6, 7, 9],
+		'TIERCE' => [1, 3, 6, 7, 9, 12],
+		'FIRST 4' => [1, 3, 6, 7, 9, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'PLACE' => [3, 9, 12],
+		'QUINELLA PLACE' => [3, 9, 12],
+		'TRIO' => [1, 3, 6, 7, 9],
+		'TIERCE' => [1, 3, 6, 7, 9, 12],
+		'FIRST 4' => [1, 3, 6, 7, 9, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'PLACE' => [3, 9, 12],
+		'QUINELLA PLACE' => [3, 9, 12],
+		'TRIO' => [1, 3, 6, 7, 9],
+		'TIERCE' => [1, 3, 6, 7, 9, 12],
+		'FIRST 4' => [1, 3, 6, 7, 9, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'PLACE' => [3, 9, 12],
+		'QUINELLA PLACE' => [3, 9, 12],
+		'TRIO' => [1, 3, 6, 7, 9],
+		'TIERCE' => [1, 3, 6, 7, 9, 12],
+		'FIRST 4' => [1, 3, 6, 7, 9, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'PLACE' => [3, 9, 12],
+		'QUINELLA PLACE' => [3, 9, 12],
+		'TRIO' => [1, 3, 6, 7, 9],
+		'TIERCE' => [1, 3, 6, 7, 9, 12],
+		'FIRST 4' => [1, 3, 6, 7, 9, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'PLACE' => [3, 9, 12],
+		'QUINELLA PLACE' => [3, 9, 12],
+		'TRIO' => [1, 3, 6, 7, 9],
+		'TIERCE' => [1, 3, 6, 7, 9, 12],
+		'FIRST 4' => [1, 3, 6, 7, 9, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+];
