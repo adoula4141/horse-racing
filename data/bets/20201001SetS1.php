a5dc915f4213def57ca3915f54934e2a614dd428
adoula4141 <elbourichi.adil@gmail.com>
2020-09-28 09:49:08 +0000

    bets for 1001

---
+++ data/bets/20201001SetS1.php
@@ -0,0 +1,124 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'PLACE' => [4],
+		'QUINELLA PLACE' => [4],
+		'TRIO' => [1, 2, 3, 5, 6],
+		'TIERCE' => [1, 2, 3, 5, 6],
+		'FIRST 4' => [1, 2, 3, 5, 6],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+];
