2c762bf7095c42bf9c9b3554666ed9d437c38a9c
adoula4141 <elbourichi.adil@gmail.com>
2020-10-26 02:28:08 +0000

    bets for 10/28

---
+++ data/bets/20201028SetS1.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'PLACE' => [1, 7, 9],
+		'QUINELLA PLACE' => [1, 7, 9],
+		'TRIO' => [1, 4, 6, 7, 9],
+		'TIERCE' => [1, 4, 6, 7, 9, 11],
+		'FIRST 4' => [1, 4, 6, 7, 9, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'PLACE' => [1, 7, 9],
+		'QUINELLA PLACE' => [1, 7, 9],
+		'TRIO' => [1, 4, 6, 7, 9],
+		'TIERCE' => [1, 4, 6, 7, 9, 11],
+		'FIRST 4' => [1, 4, 6, 7, 9, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'PLACE' => [1, 7, 9],
+		'QUINELLA PLACE' => [1, 7, 9],
+		'TRIO' => [1, 4, 6, 7, 9],
+		'TIERCE' => [1, 4, 6, 7, 9, 11],
+		'FIRST 4' => [1, 4, 6, 7, 9, 11],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+];
