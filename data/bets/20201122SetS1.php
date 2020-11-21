7745c75db23241cfbe6c42cc064d092a9ba6e966
adoula4141 <elbourichi.adil@gmail.com>
2020-11-21 06:50:27 +0000

    20201122

---
+++ data/bets/20201122SetS1.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'PLACE' => [2, 3, 4],
+		'QUINELLA PLACE' => [2, 3, 4],
+		'TRIO' => [2, 3, 4],
+		'TIERCE' => [2, 3, 4],
+		'FIRST 4' => [2, 3, 4],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'PLACE' => [2, 3, 4],
+		'QUINELLA PLACE' => [2, 3, 4],
+		'TRIO' => [2, 3, 4],
+		'TIERCE' => [2, 3, 4],
+		'FIRST 4' => [2, 3, 4],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'PLACE' => [2, 3, 4],
+		'QUINELLA PLACE' => [2, 3, 4],
+		'TRIO' => [2, 3, 4],
+		'TIERCE' => [2, 3, 4],
+		'FIRST 4' => [2, 3, 4],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+];
