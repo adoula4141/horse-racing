7d30e8b0e20c44d5cc7b818eeafaf1b6c62cd8f1
adoula4141 <59900990+adoula4141@users.noreply.github.com>
2020-11-21 14:16:46 +0800

    Update 20201122.php

--- data/racecard/20201122.php
+++ data/racecard/20201122.php
@@ -8,181 +8,49 @@ return [
 		/**
 		Race 1
 		*/
-		1 => "KTeetan",
-		2 => "ASanna",
-		3 => "HTMo(-3)",
-		4 => "SdeSousa",
-		5 => "CYHo",
-		6 => "MFPoon(-)",
+		1 => "CSchofield",
+		2 => "CLChau",
+		3 => "MLYeung",
+		4 => "TPiccone",
+		5 => "AHamelin",
+		6 => "NCallan",
 		7 => "ABadel",
-		8 => "JMoreira",
-		9 => "RBayliss",
-		10 => "URispoli",
-		11 => "BShinn",
-		12 => "KCLeung",
-		13 => "NCallan",
-		14 => "LHewitson",
+		8 => "CYHo",
+		9 => "KTeetan",
+		10 => "JMoreira",
+		11 => "KCLeung",
+		12 => "VBorged",
+		13 => "MChadwick",
 	],
 	2 => [
 		/**
 		Race 2
 		*/
-		1 => "CYHo",
-		2 => "THSo(-)",
-		3 => "NCallan",
-		4 => "RBayliss",
-		5 => "MChadwick",
-		6 => "MFPoon(-)",
-		7 => "ASanna",
-		8 => "KCLeung",
-		9 => "KTeetan",
-		10 => "HWLai",
-		11 => "JMoreira",
-		12 => "ABadel",
-		13 => "HTMo(-3)",
+		1 => "CLChau",
+		2 => "ZPurton",
+		3 => "MChadwick",
+		4 => "TPiccone",
+		5 => "JMoreira",
+		6 => "MLYeung",
+		7 => "CSchofield",
+		8 => "BShinn",
+		9 => "NCallan",
+		10 => "ABadel",
+		11 => "HNWong",
+		12 => "AHamelin",
+		13 => "CWong",
+                14 => "VBorges"
 	],
 	3 => [
 		/**
 		Race 3
 		*/
-		1 => "MFPoon(-)",
-		2 => "ASanna",
-		3 => "SdeSousa",
+		1 => "KTeetan",
+		2 => "CYHo",
+		3 => "CSchofield",
 		4 => "KCLeung",
-		5 => "KTeetan",
-		6 => "RBayliss",
-		7 => "JMoreira",
-		8 => "CYHo",
-		9 => "THSo(-)",
-		10 => "MChadwick",
-		11 => "LHewitson",
-		12 => "HNWong(-3)",
-	],
-	4 => [
-		/**
-		Race 4
-		*/
-		1 => "ASanna",
-		2 => "KTeetan",
-		3 => "URispoli",
-		4 => "BShinn",
 		5 => "ABadel",
 		6 => "JMoreira",
-		7 => "NCallan",
-		8 => "HNWong(-3)",
-		9 => "MFPoon(-)",
-		10 => "HTMo(-3)",
-		11 => "KCLeung",
-		12 => "SdeSousa",
-	],
-	5 => [
-		/**
-		Race 5
-		*/
-		1 => "ASanna",
-		2 => "KCLeung",
-		3 => "NCallan",
-		4 => "CYHo",
-		5 => "BShinn",
-		6 => "KTeetan",
-		7 => "HNWong(-3)",
-		8 => "ABadel",
-		9 => "HWLai",
-		10 => "HTMo(-3)",
-		11 => "MChadwick",
-		12 => "SdeSousa",
-		13 => "THSo(-)",
-		14 => "JMoreira",
-	],
-	6 => [
-		/**
-		Race 6
-		*/
-		1 => "SdeSousa",
-		2 => "BShinn",
-		3 => "URispoli",
-		4 => "MFPoon(-)",
-		5 => "JMoreira",
-		6 => "NCallan",
-		7 => "ABadel",
-		8 => "THSo(-)",
-		9 => "KCLeung",
-		10 => "HNWong(-3)",
-		11 => "HWLai",
-		12 => "KTeetan",
-	],
-	7 => [
-		/**
-		Race 7
-		*/
-		1 => "LHewitson",
-		2 => "BShinn",
-		3 => "ASanna",
-		4 => "KCLeung",
-		5 => "HWLai",
-		6 => "HTMo(-3)",
-		7 => "CYHo",
-		8 => "NCallan",
-		9 => "JMoreira",
-		10 => "KTeetan",
-		11 => "RBayliss",
-		12 => "ABadel",
-		13 => "MFPoon(-)",
-		14 => "MChadwick",
-	],
-	8 => [
-		/**
-		Race 8
-		*/
-		1 => "NCallan",
-		2 => "MChadwick",
-		3 => "CYHo",
-		4 => "LHewitson",
-		5 => "HTMo(-3)",
-		6 => "JMoreira",
-		7 => "KTeetan",
-		8 => "MFPoon(-)",
-		9 => "KCLeung",
-		10 => "HWLai",
-		11 => "SdeSousa",
-		12 => "ABadel",
-	],
-	9 => [
-		/**
-		Race 9
-		*/
-		1 => "KTeetan",
-		2 => "NCallan",
-		3 => "MChadwick",
-		4 => "BShinn",
-		5 => "KCLeung",
-		6 => "CYHo",
-		7 => "HWLai",
-		8 => "HTMo(-3)",
-		9 => "ASanna",
-		10 => "HNWong(-3)",
-		11 => "ABadel",
-		12 => "THSo(-)",
-		13 => "SdeSousa",
-		14 => "JMoreira",
-	],
-	10 => [
-		/**
-		Race 10
-		*/
-		1 => "KCLeung",
-		2 => "MFPoon(-)",
-		3 => "NCallan",
-		4 => "RBayliss",
-		5 => "JMoreira",
-		6 => "URispoli",
-		7 => "BShinn",
-		8 => "ABadel",
-		9 => "MChadwick",
-		10 => "HWLai",
-		11 => "CYHo",
-		12 => "SdeSousa",
-		13 => "KTeetan",
-		14 => "HNWong(-3)",
+		7 => "ZPurton",
 	],
 ];
