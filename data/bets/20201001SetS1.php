aa81d1b131eddfc11fd5b8dda10227be2fd88836
adoula4141 <elbourichi.adil@gmail.com>
2021-02-23 03:34:51 +0000

    changes

--- data/bets/20201001SetS1.php
+++ data/bets/20201001SetS1.php
@@ -7,9 +7,9 @@ return [
 		*/
 		'PLACE' => [4],
 		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
+		'TRIO' => [1, 3, 5, 6],
+		'TIERCE' => [1, 3, 5, 6],
+		'FIRST 4' => [1, 3, 5, 6],
 		'unitPlaBet' => 100,
 		'unitQplBet' => 10,
 	],
@@ -19,9 +19,9 @@ return [
 		*/
 		'PLACE' => [4],
 		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
+		'TRIO' => [1, 3, 5, 6],
+		'TIERCE' => [1, 3, 5, 6],
+		'FIRST 4' => [1, 3, 5, 6],
 		'unitPlaBet' => 100,
 		'unitQplBet' => 10,
 	],
@@ -31,93 +31,9 @@ return [
 		*/
 		'PLACE' => [4],
 		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
-	'4' => [
-		/**
-		Race 4
-		*/
-		'PLACE' => [4],
-		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
-	'5' => [
-		/**
-		Race 5
-		*/
-		'PLACE' => [4],
-		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
-	'6' => [
-		/**
-		Race 6
-		*/
-		'PLACE' => [4],
-		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
-	'7' => [
-		/**
-		Race 7
-		*/
-		'PLACE' => [4],
-		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
-	'8' => [
-		/**
-		Race 8
-		*/
-		'PLACE' => [4],
-		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
-	'9' => [
-		/**
-		Race 9
-		*/
-		'PLACE' => [4],
-		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
-	'10' => [
-		/**
-		Race 10
-		*/
-		'PLACE' => [4],
-		'QUINELLA PLACE' => [4],
-		'TRIO' => [1, 2, 3, 5, 6],
-		'TIERCE' => [1, 2, 3, 5, 6],
-		'FIRST 4' => [1, 2, 3, 5, 6],
+		'TRIO' => [1, 3, 5, 6],
+		'TIERCE' => [1, 3, 5, 6],
+		'FIRST 4' => [1, 3, 5, 6],
 		'unitPlaBet' => 100,
 		'unitQplBet' => 10,
 	],
--- data/bets/20201021SetS1.php
+++ data/bets/20201021SetS1.php
@@ -37,40 +37,4 @@ return [
 		'unitPlaBet' => 100,
 		'unitQplBet' => 10,
 	],
-	'4' => [
-		/**
-		Race 4
-		*/
-		'PLACE' => [4, 10],
-		'QUINELLA PLACE' => [4, 10],
-		'TRIO' => [1, 5, 7, 10, 11],
-		'TIERCE' => [1, 5, 7, 10, 11],
-		'FIRST 4' => [1, 5, 7, 10, 11],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
-	'5' => [
-		/**
-		Race 5
-		*/
-		'PLACE' => [4, 10],
-		'QUINELLA PLACE' => [4, 10],
-		'TRIO' => [1, 5, 7, 10, 11],
-		'TIERCE' => [1, 5, 7, 10, 11],
-		'FIRST 4' => [1, 5, 7, 10, 11],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
-	'6' => [
-		/**
-		Race 6
-		*/
-		'PLACE' => [4, 10],
-		'QUINELLA PLACE' => [4, 10],
-		'TRIO' => [1, 5, 7, 10, 11],
-		'TIERCE' => [1, 5, 7, 10, 11],
-		'FIRST 4' => [1, 5, 7, 10, 11],
-		'unitPlaBet' => 100,
-		'unitQplBet' => 10,
-	],
 ];
