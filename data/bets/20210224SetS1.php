b469c3cfd076a485426533d9a237e30b8f4f7973
adoula4141 <elbourichi.adil@gmail.com>
2021-02-23 04:20:48 +0000

    betsbets for 0224

--- data/bets/20210224SetS1.php
+++ data/bets/20210224SetS1.php
@@ -37,4 +37,76 @@ return [
 		'unitPlaBet' => 100,
 		'unitQplBet' => 10,
 	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'PLACE' => [3, 8, 12],
+		'QUINELLA PLACE' => [3, 8, 12],
+		'TRIO' => [2, 3, 4, 7, 8],
+		'TIERCE' => [2, 3, 4, 7, 8, 11, 12],
+		'FIRST 4' => [2, 3, 4, 7, 8, 11, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'PLACE' => [3, 8, 12],
+		'QUINELLA PLACE' => [3, 8, 12],
+		'TRIO' => [2, 3, 4, 7, 8],
+		'TIERCE' => [2, 3, 4, 7, 8, 11, 12],
+		'FIRST 4' => [2, 3, 4, 7, 8, 11, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'PLACE' => [3, 8, 12],
+		'QUINELLA PLACE' => [3, 8, 12],
+		'TRIO' => [2, 3, 4, 7, 8],
+		'TIERCE' => [2, 3, 4, 7, 8, 11, 12],
+		'FIRST 4' => [2, 3, 4, 7, 8, 11, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'PLACE' => [3, 8, 12],
+		'QUINELLA PLACE' => [3, 8, 12],
+		'TRIO' => [2, 3, 4, 7, 8],
+		'TIERCE' => [2, 3, 4, 7, 8, 11, 12],
+		'FIRST 4' => [2, 3, 4, 7, 8, 11, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'PLACE' => [3, 8, 12],
+		'QUINELLA PLACE' => [3, 8, 12],
+		'TRIO' => [2, 3, 4, 7, 8],
+		'TIERCE' => [2, 3, 4, 7, 8, 11, 12],
+		'FIRST 4' => [2, 3, 4, 7, 8, 11, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'PLACE' => [3, 8, 12],
+		'QUINELLA PLACE' => [3, 8, 12],
+		'TRIO' => [2, 3, 4, 7, 8],
+		'TIERCE' => [2, 3, 4, 7, 8, 11, 12],
+		'FIRST 4' => [2, 3, 4, 7, 8, 11, 12],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
 ];
