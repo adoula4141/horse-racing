9bc8a4d38583360a927f8e66066e361bf22ad135
adoula4141 <59900990+adoula4141@users.noreply.github.com>
2021-03-22 18:12:00 +0800

    Update 20191201.php

--- data/racecard/20191201.php
+++ data/racecard/20191201.php
@@ -10,10 +10,10 @@ return [
 		*/
 		1 => "KTeetan",
 		2 => "ASanna",
-		3 => "HTMo(-3)",
+		3 => "HTMo",
 		4 => "SdeSousa",
 		5 => "CYHo",
-		6 => "MFPoon(-)",
+		6 => "MFPoon",
 		7 => "ABadel",
 		8 => "JMoreira",
 		9 => "RBayliss",
@@ -28,24 +28,24 @@ return [
 		Race 2
 		*/
 		1 => "CYHo",
-		2 => "THSo(-)",
+		2 => "THSo",
 		3 => "NCallan",
 		4 => "RBayliss",
 		5 => "MChadwick",
-		6 => "MFPoon(-)",
+		6 => "MFPoon",
 		7 => "ASanna",
 		8 => "KCLeung",
 		9 => "KTeetan",
 		10 => "HWLai",
 		11 => "JMoreira",
 		12 => "ABadel",
-		13 => "HTMo(-3)",
+		13 => "HTMo",
 	],
 	3 => [
 		/**
 		Race 3
 		*/
-		1 => "MFPoon(-)",
+		1 => "MFPoon",
 		2 => "ASanna",
 		3 => "SdeSousa",
 		4 => "KCLeung",
@@ -53,9 +53,9 @@ return [
 		6 => "RBayliss",
 		7 => "JMoreira",
 		8 => "CYHo",
-		9 => "THSo(-)",
+		9 => "THSo",
 		10 => "MChadwick",
 		11 => "LHewitson",
-		12 => "HNWong(-3)",
+		12 => "HNWong",
 	],
 ];
