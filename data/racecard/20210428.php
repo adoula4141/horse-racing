c0bd8dd34db923a22e19741e3ac15c0fa3b497a8
adoula4141 <elbourichi.adil@gmail.com>
2021-04-28 00:49:39 +0000

    0428

---
+++ data/racecard/20210428.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+	 Jockey Names
+*/
+return [
+	1 => [
+		/**
+		Race 1
+		*/
+		1 => "KTeetan",
+		2 => "ASanna",
+		3 => "HTMo",
+		4 => "SdeSousa",
+		5 => "CYHo",
+		6 => "MFPoon",
+		7 => "ABadel",
+		8 => "JMoreira",
+		9 => "RBayliss",
+		10 => "URispoli",
+		11 => "BShinn",
+		12 => "KCLeung",
+		13 => "NCallan",
+		14 => "LHewitson",
+	],
+	2 => [
+		/**
+		Race 2
+		*/
+		1 => "CYHo",
+		2 => "THSo",
+		3 => "NCallan",
+		4 => "RBayliss",
+		5 => "MChadwick",
+		6 => "MFPoon",
+		7 => "ASanna",
+		8 => "KCLeung",
+		9 => "KTeetan",
+		10 => "HWLai",
+		11 => "JMoreira",
+		12 => "ABadel",
+		13 => "HTMo",
+	],
+	3 => [
+		/**
+		Race 3
+		*/
+		1 => "MFPoon",
+		2 => "ASanna",
+		3 => "SdeSousa",
+		4 => "KCLeung",
+		5 => "KTeetan",
+		6 => "RBayliss",
+		7 => "JMoreira",
+		8 => "CYHo",
+		9 => "THSo",
+		10 => "MChadwick",
+		11 => "LHewitson",
+		12 => "HNWong",
+	],
+];
