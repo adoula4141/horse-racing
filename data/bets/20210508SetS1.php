a46c922ec2666601f7139085b831bd8a82b0a24d
adoula4141 <elbourichi.adil@gmail.com>
2021-05-08 09:45:38 +0000

    0508

---
+++ data/bets/20210508SetS1.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'PLACE' => [3, 5, 6],
+		'QUINELLA PLACE' => [3, 5, 6],
+		'TRIO' => [1, 3, 5, 6, 7],
+		'TIERCE' => [1, 3, 5, 6, 7, 8, 9],
+		'FIRST 4' => [1, 3, 5, 6, 7, 8, 9],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'PLACE' => [3, 5, 6],
+		'QUINELLA PLACE' => [3, 5, 6],
+		'TRIO' => [1, 3, 5, 6, 7],
+		'TIERCE' => [1, 3, 5, 6, 7, 8, 9],
+		'FIRST 4' => [1, 3, 5, 6, 7, 8, 9],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'PLACE' => [3, 5, 6],
+		'QUINELLA PLACE' => [3, 5, 6],
+		'TRIO' => [1, 3, 5, 6, 7],
+		'TIERCE' => [1, 3, 5, 6, 7, 8, 9],
+		'FIRST 4' => [1, 3, 5, 6, 7, 8, 9],
+		'unitPlaBet' => 100,
+		'unitQplBet' => 10,
+	],
+];
